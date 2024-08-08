c9031035784e32502a9821842bb8b6b862ddeb26
Rahul_Rathour <rahulrathor0988@gmail.com>
2024-08-08 20:04:55 +0530

    front

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import AdminRegister from './components/AdminRegister';
+import AdminSignin from './components/AdminSignin';
 
 function App() {
   return (
     <div className="App">
        <Home/>
+       <AdminRegister/>
+       <AdminSignin/>
     </div>
   );
 }
